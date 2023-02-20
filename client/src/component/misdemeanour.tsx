47d91602f77c09010b3a50ee8ce22ac07823fdee
cx9s <cx9918@gmail.com>
2023-02-20 18:50:43 +0000

    Misdemeanour shows misdemeanours

--- client/src/component/misdemeanour.tsx
+++ client/src/component/misdemeanour.tsx
@@ -1,3 +1,30 @@
-const Misdemeanour: React.FC = () => <>Misdemeanour!</>;
+import { useEffect, useState } from "react";
+import { MisdemeanourDataType } from "../../types/misdemeanours.types";
+import MisList from "./misList";
+
+const Misdemeanour: React.FC = () => {
+  const fetchUrl = "http://localhost:8080/api/misdemeanours/10";
+  const [misdemeanours, setMisdemeanour] = useState<MisdemeanourDataType[]>([]);
+
+  const fetchMisdemeanours = async (url: string) => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      setMisdemeanour(json.misdemeanours);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchMisdemeanours(fetchUrl);
+  }, []);
+
+  return (
+    <div>
+      <MisList misList={misdemeanours} />
+    </div>
+  );
+};
 
 export default Misdemeanour;
