c4ecd44c54843ae121eb930c968d99a62a03983d
cx9s <cx9918@gmail.com>
2023-02-21 02:37:48 +0000

    add Subject input with onChange()

--- client/src/component/subject.tsx
+++ client/src/component/subject.tsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+
 interface SubjectProps {
   subject: string;
   setSubject: (subject: string) => void;
 }
 
+const validSubject = (value: string) => {
+  return /^[a-zA-Z]{1,50}$/.test(value);
+};
+
 const Subject: React.FC<SubjectProps> = ({ subject, setSubject }) => {
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(
+    undefined
+  );
+
+  const onChangeHandler = (value: string) => {
+    const validated = validSubject(value);
+    const message = validated
+      ? undefined
+      : "Must be between 1 and 50 characters.";
+    setErrorMessage(message);
+    setSubject(value);
+  };
+
   return (
     <div>
       <label htmlFor="subject">Subject: </label>
@@ -13,9 +32,10 @@ const Subject: React.FC<SubjectProps> = ({ subject, setSubject }) => {
         placeholder="Please input subject"
         value={subject}
         onChange={(e) => {
-          setSubject(e.target.value);
+          onChangeHandler(e.target.value);
         }}
       />
+      <span className="error">{errorMessage}</span>
     </div>
   );
 };
