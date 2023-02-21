9070b331bb6dc1c468cc5788b74453a2940c4c86
cx9s <cx9918@gmail.com>
2023-02-21 02:00:49 +0000

    add form and subject input

--- client/src/component/confession.tsx
+++ client/src/component/confession.tsx
@@ -1,3 +1,21 @@
-const Confession: React.FC = () => <>Confession!</>;
+import Subject from "./subject";
+
+const Confession: React.FC = () => {
+  return (
+    <>
+      <p className="confession__p">
+        It's very difficult to catch people committing misdemeanours, so we
+        appreciate it when citizens confess to us directly.
+      </p>
+      <p className="confession__p">
+        However, if you're just having a hard day and need to vent then you're
+        welcome to contact us here too. Up to you!
+      </p>
+      <form className="form">
+        <Subject />
+      </form>
+    </>
+  );
+};
 
 export default Confession;
---
+++ client/src/component/subject.tsx
@@ -0,0 +1,13 @@
+const Subject: React.FC = () => (
+  <div>
+    <label htmlFor="subject">Subject: </label>
+    <input
+      id="subject"
+      type="text"
+      placeholder="Please input subject"
+      value=""
+    />
+  </div>
+);
+
+export default Subject;
