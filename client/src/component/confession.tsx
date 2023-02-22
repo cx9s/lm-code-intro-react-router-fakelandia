a990630de2aa157789f540bc3e760c27ec77a432
cx9s <cx9918@gmail.com>
2023-02-22 15:58:04 +0000

    add subject, reason and detials in form

--- client/src/component/confession.tsx
+++ client/src/component/confession.tsx
@@ -1,8 +1,38 @@
 import { useState } from "react";
-import Subject from "./subject";
+import { TextInput } from "./confessionForm/textInput";
+import {
+  ConfessionFormChangeHandler,
+  ConfessionFormData,
+} from "./confessionForm/confessionForm.types";
+import {
+  validateDetails,
+  validateReason,
+  validateSubject,
+} from "./confessionForm/validateForm";
+import { SelectInput } from "./confessionForm/selectInput";
+
+const defaultFormData: ConfessionFormData = {
+  subject: "",
+  reason: "NOT_SELECTED",
+  details: "",
+};
 
 const Confession: React.FC = () => {
-  const [subject, setSubject] = useState<string>("");
+  const [formData, setFormData] = useState<ConfessionFormData>(defaultFormData);
+
+  const onChangeHandler: ConfessionFormChangeHandler = <
+    TKey extends keyof ConfessionFormData
+  >(
+    value: ConfessionFormData[TKey],
+    name: TKey
+  ) => {
+    setSubmitted(false);
+    const newData: ConfessionFormData = { ...formData };
+    newData[name] = value;
+    setFormData(newData);
+  };
+
+  const [submitted, setSubmitted] = useState(false);
 
   return (
     <>
@@ -14,8 +44,42 @@ const Confession: React.FC = () => {
         However, if you're just having a hard day and need to vent then you're
         welcome to contact us here too. Up to you!
       </p>
-      <form className="form">
-        <Subject subject={subject} setSubject={setSubject} />
+      <form className="confessionForm">
+        <TextInput
+          id="subject"
+          type="text"
+          name="subject"
+          value={formData.subject}
+          placeholder="Enter subject"
+          label="Subject"
+          validate={validateSubject}
+          onChangeHandler={onChangeHandler}
+        />
+        <SelectInput
+          id="reason"
+          name="reason"
+          value={formData.reason ?? ""}
+          label="Reason"
+          validate={validateReason}
+          onChangeHandler={onChangeHandler}
+          options={[
+            { value: "NOT_SELECTED", display: "-" },
+            { value: "rudeness", display: "Mild Public Rudeness" },
+            { value: "vegetables", display: "Speaking in a Lift" },
+            { value: "lift", display: "Not Eating Your Vegetables" },
+            { value: "united", display: "Supporting Manchester United" },
+          ]}
+        />
+        <TextInput
+          id="details"
+          type="textarea"
+          name="details"
+          value={formData.details}
+          placeholder="Enter details"
+          label="Details"
+          validate={validateDetails}
+          onChangeHandler={onChangeHandler}
+        />
       </form>
     </>
   );
