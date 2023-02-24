ab9f810a2b41bfd33503c3e1ebf5955b42662274
cx9s <cx9918@gmail.com>
2023-02-24 12:14:06 +0000

    add loading

--- client/src/component/misdemeanour.tsx
+++ client/src/component/misdemeanour.tsx
@@ -11,6 +11,7 @@ const Misdemeanour: React.FC = () => {
   const fetchUrl = "http://localhost:8080/api/misdemeanours/10";
   const [misSelect, setMisSelect] = useState<MisdemeanourKind | "">("");
   const [misdemeanours, setMisdemeanour] = useState<MisdemeanourDataType[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const { confessions } = useContext(
     ConfessionContext
@@ -24,6 +25,7 @@ const Misdemeanour: React.FC = () => {
         const response = await fetch(url);
         const json = await response.json();
         setMisdemeanour(json.misdemeanours);
+        setIsLoading(false);
       } catch (e) {
         console.error(e);
       }
@@ -36,12 +38,26 @@ const Misdemeanour: React.FC = () => {
 
   return (
     <>
-      <MisSelect setMis={setMisSelect} />
-      <MisList
-        misList={misdemeanours.filter((mis) =>
-          mis.misdemeanour.includes(misSelect)
-        )}
-      />
+      {isLoading && (
+        <div className="isLoading">
+          <div className="container">
+            <div className="item item-1">rudeness</div>
+            <div className="item item-2">vegetables</div>
+            <div className="item item-3">lift</div>
+            <div className="item item-4">united</div>
+          </div>
+        </div>
+      )}
+      {!isLoading && (
+        <>
+          <MisSelect setMis={setMisSelect} />
+          <MisList
+            misList={misdemeanours.filter((mis) =>
+              mis.misdemeanour.includes(misSelect)
+            )}
+          />
+        </>
+      )}
     </>
   );
 };
