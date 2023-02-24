685e4bb94fda23355f767386d26a6144e804c862
cx9s <cx9918@gmail.com>
2023-02-24 17:54:21 +0000

    setup jest for vite app

---
+++ client/jest.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  extensionsToTreatAsEsm: [".ts"],
+  setupFilesAfterEnv: ["./src/setupTests.ts"],
+  testEnvironment: "jsdom",
+};
+
+export default config;
