53fdf351ec8abc15bf3a925bd5ed10b442ab51a0
cx9s <cx9918@gmail.com>
2023-03-01 00:56:25 +0000

    add MisChart in home page

---
+++ client/src/component/home/misChart.tsx
@@ -0,0 +1,36 @@
+import { Chart } from "react-google-charts";
+
+export interface MisChartProps {
+  rudeness: number;
+  vegetables: number;
+  lift: number;
+  united: number;
+}
+
+const MisChart: React.FC<MisChartProps> = ({
+  rudeness = 0,
+  vegetables = 0,
+  lift = 0,
+  united = 0,
+}) => {
+  const data = [
+    ["Misdemeanour kind", "numbers in total"],
+    ["Rudeness", rudeness],
+    ["Vegetables", vegetables],
+    ["Lift", lift],
+    ["United", united],
+  ];
+
+  const total = rudeness + vegetables + lift + united;
+
+  const options = {
+    title: `Misdemeanours: ${total}`,
+    // with: 500,
+    height: 500,
+    colors: ["#fc891c", "#1a1a1a", "#008842", "#c70101"],
+  };
+
+  return <Chart chartType="PieChart" data={data} options={options} />;
+};
+
+export default MisChart;
