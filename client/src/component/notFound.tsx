d30141e92b1a02d9a0f52ef083b1da2fac9916e4
cx9s <cx9918@gmail.com>
2023-03-01 16:26:21 +0000

    style NotFound

--- client/src/component/notFound.tsx
+++ client/src/component/notFound.tsx
@@ -1,3 +1,29 @@
-const NotFound: React.FC = () => <>NotFound!</>;
+import notFoundImage from "../assets/notFoundImage.jpg";
+import { useNavigate } from "react-router-dom";
+
+const NotFound: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="notFoundContainer">
+      <img className="notFound__img" src={notFoundImage} />
+      <div className="notFound__text">
+        <h2 className="notFound__title">Are you lost in mountains?</h2>
+        <p className="notFound__p">
+          Follow me to the{" "}
+          <a
+            className="notFound__a"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Fakelandia
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default NotFound;
