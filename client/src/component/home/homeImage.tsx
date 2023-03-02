ed21f77d6f8c1f591dec29b36d0aa04f9541a89e
cx9s <cx9918@gmail.com>
2023-03-02 17:02:08 +0000

    move style into .css

--- client/src/component/home/homeImage.tsx
+++ client/src/component/home/homeImage.tsx
@@ -2,7 +2,7 @@ import image from "../../assets/homeImage.jpg";
 
 const HomeImage: React.FC = () => (
   <div className="homeImage">
-    <img src={image} />
+    <img src={image} className="homeImage__img" />
     <p className="fakelandia">Welcome to Fakelandia.</p>
   </div>
 );
